e9f15d3bdd2972511fae40c376417c50f36e2789
bbggkkk <bbggkkk@naver.com>
2024-10-04 14:24:17 +0900

    필요없는 주석 정리

--- build/build.setting.js
+++ build/build.setting.js
@@ -1,11 +1,7 @@
 export default {
     entryPoints: [
         'index.ts',
-        // 'src/styles/variables.scss'
     ],
-    // loader: {
-    //     '.scss': 'css'
-    // },
     bundle: true,
     outdir: 'dist',
     minify: true
