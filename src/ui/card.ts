b19a70fd876d5bc445d2031365b7fcadcfd627b5
bbggkkk <bbggkkk@naver.com>
2024-10-04 18:19:21 +0900

    테마와 카드 분리

--- src/ui/card.ts
+++ src/ui/card.ts
@@ -37,16 +37,17 @@ export abstract class HanaUiCard<T extends HanaUiCardConfig> extends LitElement 
         --hana-ui-card-padding-base: calc(var(--hana-ui-padding-unit) * 3);
         --hana-ui-card-radius-base: calc(var(--hana-ui-card-padding-base) * 2);
 
-        --hana-ui-card-border: var(--hana-ui-card-border-width, 1px) solid var(--divider-color);
-        --hana-ui-card-gap: calc(var(--hana-ui-card-padding) / 4);
         --hana-ui-card-background: hsl(var(--hana-ui-gray-base) 94%);
     }
     :host {
         --ha-card-background: hsl(var(--hana-ui-gray-base) 94%);
     }
+    
     /* always hana-ui style */
     :host {
         --hana-ui-card-padding: var(--hana-ui-card-padding-base, 24px);
+        --hana-ui-card-gap: calc(var(--hana-ui-card-padding) / 4);
+        --hana-ui-card-border: var(--hana-ui-card-border-width, 1px) solid var(--divider-color);
         --hana-ui-card-radius: var(--hana-ui-card-radius-base, 12px);
     }
     /* always override style */
